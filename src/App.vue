d084385b41b4b2469f5b3b19957d58c448b83a25
Carlos Castro <carlos@trabajoendigital.com>
2020-05-10 00:46:28 -0500

    new background

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,25 @@ export default {
   name: "App",
   components: {
     HelloWorld
+  },
+  mounted() {
+    var imagen = document.getElementById("app");
+    imagen.style.backgroundImage = "url('./img/478048.jpg')";
   }
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+#app {
+  background-position: center;
+  background-size: cover;
+  /* background-repeat: repeat; */
 }
 </style>
